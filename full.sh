4e39702b9e540c2a96604465035662581729d089
Elian Manzueta <eliandmanzueta@gmail.com>
2023-11-27 02:45:43 -0500

    fixed some errors in full.sh

--- full.sh
+++ full.sh
@@ -1,7 +1,7 @@
 CONFIG_DIR="$HOME/dotfiles"
 ZSHRC="$CONFIG_DIR/.zsh/.zshrc"
-ZSH_THEME="$CONFIG_DIR/.zsh/.p10k.zsh"
-BASHRC="$CONFID_DIR/.bashrc"
+ZSH_THEME="$CONFIG_DIR/.zsh/themes"
+BASHRC="$CONFIG_DIR/.bashrc"
 NEOVIM="$HOME/.config/nvim"
 
 if [ "$OSTYPE" = "linux-gnu" ]; then
@@ -10,26 +10,35 @@ if [ "$OSTYPE" = "linux-gnu" ]; then
 	sudo apt-get install curl
 fi
 
-# Install Oh My Zsh!
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install Oh My Zsh if not already installed
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
 # Install P10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
 
-# Instll ZSH Plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions             # Autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting # Syntax Highlighting
+# Install ZSH Plugins
+if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
 
 # Clone neovim repo
 echo "\nCloning neovim repository..."
-mkdir $HOME/.config/nvim
-git clone https://github.com/elianmanzueta/nvim $NEOVIM
+if [ ! -d "$NEOVIM" ]; then
+	mkdir -p $NEOVIM
+	git clone https://github.com/elianmanzueta/nvim $NEOVIM
+fi
 
 # Configure symlinks
 echo "\nConfiguring symlinks..."
 ln -fs $ZSHRC $HOME/.zshrc
 ln -fs $BASHRC $HOME/.bashrc
-ln -fs $ZSH_THEME $HOME/.p10k.zsh
 
 # Configure gitconfig
 echo "\nConfiguring gitconfig..."
@@ -38,16 +47,16 @@ git config --global user.email "eliandmanzueta@gmail.com"
 git config --global color.ui auto
 
 # Prompt user for confirmation
-read -p "Install Homebrew? (y\n) " confirmation
+read -p "Install Homebrew? (y/n) " confirmation
 
-if [[ "$confirmation" == [yY]* ]]; then
+if [ "$confirmation" = "y" ] || [ "$confirmation" = "Y" ]; then
 	# Install Homebrew
 	echo "\nInstalling Homebrew..."
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 	# Installing stuff
 	brew install neovim
 	brew install aichat
-	if [ "$OSTYPE" = "darwin"* ]; then
+	if [[ "$OSTYPE" =~ "darwin" ]]; then
 		brew install zsh
 		brew install curl
 		brew install xclip
@@ -59,7 +68,6 @@ fi
 
 if [ "$OSTYPE" = "linux-gnu" ]; then
 	sudo apt-get install wget snmp snmpd snmp-mibs-downloader fail2ban cmatrix unzip libfuse2 ninja-build gettext cmake zsh xclip
-
 fi
 
 # Add ZSH to /etc/shells
@@ -67,5 +75,3 @@ echo "\nAdding ZSH to allowed shells..."
 command -v zsh | sudo tee -a /etc/shells
 
 echo "Run chsh to change shell to ZSH."
-
-echo "Complete."
