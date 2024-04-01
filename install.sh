87fd7bfaffdd431a5cfb3cba18e9e88d0c55b461
Elian Manzueta <eliandmanzueta@gmail.com>
2024-04-01 13:38:45 -0400

    added p10k install
    Signed-off-by: Elian Manzueta <eliandmanzueta@gmail.com>

--- install.sh
+++ install.sh
@@ -16,8 +16,11 @@ else
 fi
 
 # Install Oh My Zsh! Plugins
-
 echo "Installing plugins"
+
+# P10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 # Zsh Syntax Highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
