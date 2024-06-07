7a18317464b2bc367e4833fdf8fdd71f9dd89c1e
Elian Manzueta <eliandmanzueta@gmail.com>
2024-06-07 10:59:19 -0400

    cleaned up install.sh
    Signed-off-by: Elian Manzueta <eliandmanzueta@gmail.com>

--- install.sh
+++ install.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-# Variables
-DIR=$PWD
+# Vars
+DIR="$HOME/dotfiles"
 
-# Install Oh My Zsh!
+# Install Oh My Zsh + Plugins
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Install Oh My Zsh! Plugins
-echo "Installing plugins"
-
-# P10k
+## P10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# Zsh Syntax Highlighting
+## Zsh Syntax Highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# Zsh Autosuggestions
+## Zsh Autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Symlinks
-echo "Configuring symlinks"
 ln -fs $DIR/.zshrc $HOME/.zshrc
 ln -fs $DIR/.p10k.zsh $HOME/.p10k.zsh
 ln -fs $DIR/.gitconfig $HOME/.gitconfig
@@ -29,7 +25,7 @@ ln -fs $DIR/.tmux.conf $HOME/.tmux.conf
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 export PATH="/opt/homebrew/bin:$PATH"
 
-# Install brew packages
+## Install brew packages
 brew install gh
 brew install neovim
 brew install tree
@@ -39,14 +35,12 @@ brew install tmux
 brew install pyenv
 brew install pyenv-virtualenv
 
-# Install pyenv pyright
+# Install pyenv-pyright
 git clone https://github.com/alefpereira/pyenv-pyright.git $(pyenv root)/plugins/pyenv-pyright
 
-# Neovim config
+# Neovim Configuration
 if [ ! -d "$HOME/.config/" ]; then
 	mkdir -p "$HOME/.config/"
 fi
 
 git clone "https://github.com/elianmanzueta/nvim" $HOME/.config/nvim
-
-echo "Complete"
