a5d15ae6345e90472a9f4dd6088920b9784058aa
Elian Manzueta <eliandmanzueta@gmail.com>
2024-06-10 12:09:26 -0400

    enabled unattended upgrades to ubuntu-install.sh
    Signed-off-by: Elian Manzueta <eliandmanzueta@gmail.com>

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -67,3 +67,7 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
+
+# Unattended Upgrades
+sudo apt-get install unattended-upgrades -y
+sudo dpkg-reconfigure -plow unattended-upgrades
