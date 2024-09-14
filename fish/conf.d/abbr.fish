3cb63b67414caf7651e054f37d941445229f84c4
Elian Manzueta <eliandmanzueta@gmail.com>
2024-09-13 23:56:40 -0400

    added fish
    Signed-off-by: Elian Manzueta <eliandmanzueta@gmail.com>

---
+++ fish/conf.d/abbr.fish
@@ -0,0 +1,6 @@
+# Git
+abbr --add gst git status
+abbr --add gcsm git commit --signed --message
+abbr --add gp git push
+abbr --add ga git add
+
--- scripts/cachy.sh
+++ scripts/cachy.sh
@@ -11,8 +11,8 @@ cd paru || exit
 makepkg -si
 
 # Symlinks
-ln -fs "$DIR"/.zshrc "$HOME"/.zshrc
-ln -fs "$DIR"/.p10k.zsh "$HOME"/.p10k.zsh
+ln -fs "$DIR"/zsh/.zshrc "$HOME"/.zshrc
+ln -fs "$DIR"/zsh/.p10k.zsh "$HOME"/.p10k.zsh
 
 # Install packages
 paru -S neovim ripgrep fd eza zoxide bat github-cli zsh curl kitty wl-clipboard steam stremio fzf bolt-launcher lutris git
