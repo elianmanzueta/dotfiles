378d04997d5e5895da66b447df78b7676141807d
Elian Manzueta <eliandmanzueta@gmail.com>
2024-09-15 22:43:20 -0400

    added more fish stuff
    Signed-off-by: Elian Manzueta <eliandmanzueta@gmail.com>

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -4,3 +4,16 @@ abbr --add gcsm git commit --signoff --message
 abbr --add gp git push
 abbr --add ga git add
 
+# Cargo
+abbr --add cr cargo run
+
+# Ls
+abbr --add ls exa -lha --color=auto --icons=always
+abbr --add lt exa --tree
+
+# Config
+abbr --add fishconfig nvim ~/.config/fish/
+abbr --add starshipconfig nvim ~/.config/starship.toml
+
+# Zoxide
+abbr --add cd z
