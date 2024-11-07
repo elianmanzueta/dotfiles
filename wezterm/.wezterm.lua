50ef59dc4f6f2001ff0c7ebe86b2c0989f9b25fe
Elian Manzueta <elianmanzueta@protonmail.com>
2024-11-07 18:04:28 -0500

    update wezterm.conf
    Signed-off-by: Elian Manzueta <elianmanzueta@protonmail.com>

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,8 +1,15 @@
 local wezterm = require("wezterm")
 local config = {}
 
+--x86_64-pc-windows-msvc - Windows
+-- x86_64-apple-darwin - macOS (Intel)
+-- aarch64-apple-darwin - macOS (Apple Silicon)
+-- x86_64-unknown-linux-gnu - Linux
+
 -- WSL
-config.default_domain = "WSL:Arch"
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.default_domain = "WSL:Arch"
+end
 
 -- Apperance
 config.color_scheme = "duckbones"
@@ -13,7 +20,7 @@ config.keys = {
 	-- Panes
 	{ key = "s", mods = "CTRL|ALT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
 	{ key = "v", mods = "CTRL|ALT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "w", mods = "CTRL|ALT", action = wezterm.action.CloseCurrentPane { confirm = false } },
+	{ key = "w", mods = "CTRL|ALT", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
 }
 
 return config
